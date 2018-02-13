8c52e34efa9e6b32e0da57b3e9a05583151e6018
samstudy <samat.study@gmail.com>
2018-02-13 21:28:49 +0600

    unused projects

--- transcendence/first_iteration/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FirstIterationConfig(AppConfig):
-    name = 'first_iteration'
--- transcendence/first_iteration/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import UserInformation
-
-
-def user_profile(request, pk):
-    user = UserInformation.objects.get(pk=pk)
-    return render(request, 'user.html', {'user': user})
