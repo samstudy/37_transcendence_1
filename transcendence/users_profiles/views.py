644ef8e05e76ebe683aa606c82cd9a92b19ec7be
samstudy <samat.study@gmail.com>
2018-02-13 22:27:46 +0600

    changed template name

--- transcendence/users_profiles/views.py
+++ transcendence/users_profiles/views.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 class UserPageView(DetailView):
     model = User
-    template_name = "home.html"
+    template_name = "user.html"
 
     def get_context_data(self, **kwargs):
         context = super(UserPageView, self).get_context_data(**kwargs)
-        context['users'] = User.objects.filter(username=self.get_object())
+        context['user'] = User.objects.filter(username=self.get_object())
         return context
