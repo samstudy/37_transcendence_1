a00e5a3dc7d643e9e2658f5fa63ba5c15586f555
samstudy <samat.study@gmail.com>
2018-02-25 18:32:17 +0600

    Added STATIC_ROOT for deploying static files

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -88,7 +88,7 @@ class Base(Configuration):
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
